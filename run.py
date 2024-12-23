f96c7e2150efd29f4b9110cbca30783739ac4434
grt77 <36742299+grt77@users.noreply.github.com>
2024-12-23 16:45:32 +0530

    Update run.py

--- run.py
+++ run.py
@@ -21,4 +21,4 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
     app.logger.setLevel(logging.DEBUG)
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
